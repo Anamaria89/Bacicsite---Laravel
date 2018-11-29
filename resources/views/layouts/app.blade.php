59881fa9dcdd833d4a8cf71ccd42e3970786d03e
Anamaria <amic_dragic@yahoo.com>
2018-11-29 15:20:17 +0100

    Laravel website

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Ama</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        @include('inc.navbar')
+        <div class="container">
+            @if(Request::is('/'))
+                @include('inc.showcase')
+            @endif
+            <div class="row">
+                <div class="col-md-8 col-lg-8">
+                    @include('inc.messages')
+                    @yield('content')
+                </div>
+               <div class="col-md-4 col-lg-4">
+                     @include('inc.sidebar')
+                </div>
+            </div>
+        </div>
+        <div id="footer" class="text-center">
+            <p>Copyright 2018 &copy; Ama</p>
+        </div>
+       <script src="{{ asset('js/app.js') }}"></script> 
+    </body>
+</html>
